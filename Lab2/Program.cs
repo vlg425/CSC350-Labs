1e8cfd1b3112ff7742fb8f1c10aa644c780d75f8
vlg425 <114525880+vlg425@users.noreply.github.com>
2025-09-04 21:04:51 -0400

    added contructors and add student function

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,12 +1,61 @@
-﻿class StudentGroup
+﻿//*************************************************
+//Authors: Victor Garcia, Tiffany Turner
+//CSC350
+//StudentGroups Lab 2
+//*************************************************
+
+class StudentGroup
 {
     private string groupName;
-    private string[] members;
-    private int numberOfMembers;
+    private string[] studentNames;
+    private int maxGroupSize; //number of students in group
+    private int currentStudentCount; //current number of students in group
 
-    public StudentGroup();
-    public StudentGroup(string groupName, int size);
-    public addMember(string name);
-    public getMember(int position);
-    public displayGroup();
+
+    //Constructors
+    public StudentGroup()
+    {
+        groupName = "Team Alpha";
+        maxGroupSize = 3;
+        currentStudentCount = 0;
+        studentNames = new string[maxGroupSize];
+    }
+
+
+    public StudentGroup(string groupName, int maxGroupSize)
+    {
+        this.groupName = groupName;
+        this.maxGroupSize = maxGroupSize;
+        currentStudentCount = 0;
+        studentNames = new string[maxGroupSize];
+    }
+
+    //Add student function: pass a name and it gets added to student Names array, incremients current student count
+    public void AddStudent(string name)
+    {
+        if (currentStudentCount < maxGroupSize)
+        {
+            studentNames[currentStudentCount] = name;
+            currentStudentCount++;
+        }
+        else
+        {
+            Console.WriteLine("Cannot add more students; group is full.");
+        }
+    }
+
+
+    public void GetStudent(int position)
+    {
+        
+    }
+
+
+    public void ShowStudents()
+    {
+       
+    }
 }
+
+
+
