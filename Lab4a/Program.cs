83b8fb6110142e6ac363cf429025b26a14ba259d
Victor Garcia <114525880+vlg425@users.noreply.github.com>
2025-09-16 14:35:41 -0400

    init commit player UML and added cards2.dll to bin

---
+++ Lab4a/Program.cs
@@ -0,0 +1,26 @@
+﻿//UML
+//Player()
+//--------------------
+//-playerID : int
+//-playerCards[] : Card[]
+//
+//--------------------
+//+Player()
+//+getPlayerID() : int
+//+setPlayerID(int) : void
+//+getNumberofCards() : int
+
+using Cards2;
+
+class Player
+{
+
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
