52213d3ba61e7bcb0f255bce7ce906bd7b1ec2f6
Victor Garcia <114525880+vlg425@users.noreply.github.com>
2025-09-16 15:33:37 -0400

    crated basic player class and methods

--- Lab4a/Program.cs
+++ Lab4a/Program.cs
@@ -1,26 +1,90 @@
 ﻿//UML
 //Player()
 //--------------------
+//-playerCards : List<Card>
 //-playerID : int
-//-playerCards[] : Card[]
+//--------------------
+//+GetPlayerID() : int
+//+AddCard(card : Card) : void
+//+RemoveCard(cardIndex : int) : Card
+//+GetCardCount() : int
+//+HasCards() : bool
+//+DisplayCards() : void
+//+SortCardsByValue() : void
+//+SortCardsBySuit() : void
 //
 //--------------------
-//+Player()
-//+getPlayerID() : int
-//+setPlayerID(int) : void
-//+getNumberofCards() : int
 
 using Cards2;
-
+using System;
 class Player
 {
+    private List<Card> playerCards;
+
+    public Player()
+    {
+        playerCards = new List<Card>();
+
+    }
+
+
+
+    public void AddCard(Card card)
+    {
+        playerCards.Add(card);
+    }
+
+    public Card RemoveCard(int cardIndex)
+    {
+        if (cardIndex < 0 || cardIndex >= playerCards.Count)
+        {
+            throw new ArgumentOutOfRangeException("Invalid card index");
+        }
+        Card removedCard = playerCards[cardIndex];
+        playerCards.RemoveAt(cardIndex);
+        return removedCard;
+    }
+
+    public int GetCardCount()
+    {
+        return playerCards.Count;
+    }
+
+    public bool HasCards()
+    {
+        return playerCards.Count > 0;
+    }
+
+    public void DisplayCards()
+    {
+        foreach (var card in playerCards)
+        {
+            Console.WriteLine(card.Rank + " of " + card.Suit);
+
+        }
+    }
+    
+    
+
+
 
 }
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Player player = new Player();
+        Deck deck = new Deck();
+        deck.Shuffle();
+
+        player.AddCard(deck.TakeTopCard());
+        player.AddCard(deck.TakeTopCard());
+        player.AddCard(deck.TakeTopCard());
+        player.AddCard(deck.TakeTopCard());
+        player.DisplayCards();
+        
+
     }
+      
 }
 
